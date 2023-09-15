a6748187cc119330e55782d895e766fa1fbc46b8
daisy <faisykemmy@gmail.com>
2023-09-15 15:24:43 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul- function that checks for a dgit (0 though)
+ * @a: is a variable
+ * @b: is a variable
+ * Return: Always 0
+ */
+
+int mul(int a, int b)
+{
+	int mul;
+
+	mul = a * b;
+	return (mul);
+}
